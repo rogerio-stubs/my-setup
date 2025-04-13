2a715997c62ae2b0f9ce7af0aa64262eecc249ca
rogerio-stubs <rostubs17@gmail.com>
2025-04-13 02:00:46 -0300

    criando scritps

---
+++ Developer/install-developer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Git
+sudo apt install git 
+
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+
+# beekeper 
+
+# vscode 
+
+# Cursor AI
+
+
+source ~/.bashrc
+
+
+
---
+++ Github/install-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+# Create SSH Key
